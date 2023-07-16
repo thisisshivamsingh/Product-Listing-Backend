2054d89ff6443f7d333325cbe23b9ec558320139
Shivam <thisisshivam06@gmail.com>
2023-07-17 00:53:36 +0530

    Final Commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,7 +11,7 @@ const createSendToken = (apiKey, statusCode, res) => {
   const token = signToken(apiKey);
   const cookieOptions = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 60 * 60 * 1000
     ),
     httpOnly: true,
   };
